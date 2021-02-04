051e45d8cfb19586a4d9f37254412ab5f9774f39
vitoraugustto <60718553+vitoraugustto@users.noreply.github.com>
2021-02-04 13:02:32 -0300

    update

--- bot/botCMSPgo.py
+++ bot/botCMSPgo.py
@@ -20,6 +20,7 @@ horario = input("Digite o horário: ")
 print("_________________________________________")
 
 def botCMSP():
+
     print("Bot rodando...")
     
     os.startfile('C:\Program Files\IP.TV Studio HD 518\iptv.exe')
@@ -27,45 +28,62 @@ def botCMSP():
 
     iptvButton = pyautogui.locateCenterOnScreen('references/iptvbutton.png')
     pyautogui.click(iptvButton)
-    time.sleep(3)
 
-    iptvwindow = pyautogui.locateCenterOnScreen('references/iptvbutton.png', confidence=0.7)
+    iptvwindow = pyautogui.locateCenterOnScreen('references/iptvwindow.png', grayscale=True, confidence=0.8)
 
-    if not (iptvwindow):
+    k = 0
+    while iptvwindow == None:
+        iptvwindow = pyautogui.locateCenterOnScreen('references/iptvwindow.png', confidence=0.8)
+        k += 1 
+        print(iptvwindow, k)
+        time.sleep(0.1)
+
+        if ((iptvwindow) is not None):
         
-        pyautogui.typewrite('treinamento')
-        time.sleep(0.5)
+            pyautogui.typewrite('treinamento')
+            time.sleep(0.5)
 
-        pyautogui.press('tab', presses=4)
-        pyautogui.press('up')
-        pyautogui.press('enter')
-        time.sleep(3)
+            pyautogui.press('tab', presses=4)
+            pyautogui.press('up')
+            pyautogui.press('enter')
 
-        iptvUsers = pyautogui.locateCenterOnScreen('references/iptvusers.png', confidence=0.8)
-        pyautogui.click(iptvUsers)
-        time.sleep(3)
+            iptvblackwindow = pyautogui.locateCenterOnScreen('references/iptvblackwindow.png', grayscale=True, confidence=0.8)
 
-        iptvsptransmit = pyautogui.locateCenterOnScreen('references/iptvsptransmit.png', confidence=0.8)
-        pyautogui.click(iptvsptransmit)
-        pyautogui.keyDown('ctrl')
-        pyautogui.press('v')
-        pyautogui.keyUp('ctrl')
-        time.sleep(3)
+            i = 0
+            while iptvblackwindow == None:
+                iptvblackwindow = pyautogui.locateCenterOnScreen('references/iptvblackwindow.png', confidence=0.8)
+                i += 1 
+                print(iptvblackwindow, i)
+                time.sleep(0.1)
 
-        playlistButton = pyautogui.locateCenterOnScreen('references/playlistbutton.png', confidence=0.8)
-        pyautogui.click(playlistButton)
-        time.sleep(3)
+                if ((iptvblackwindow) is not None):
 
-        pyautogui.keyDown('win')
-        pyautogui.press('1')
-        pyautogui.keyUp('win')
-        
-        # sairButton = pyautogui.locateCenterOnScreen('references/sairButton.png')
-        # pyautogui.click(sairButton)
-        # time.sleep(1)
-        
-        print("Programação concluída com sucesso! ft. Vitão, Agnaldão and Betão.")
-        pyautogui.alert(text="Programação concluída com sucesso! ft. Vitão, Agnaldão and Betão.")
+                    iptvUsers = pyautogui.locateCenterOnScreen('references/iptvusers.png', confidence=0.8)
+                    pyautogui.click(iptvUsers)
+                    time.sleep(0.3)
+
+                    iptvsptransmit = pyautogui.locateCenterOnScreen('references/iptvsptransmit.png', confidence=0.8)
+                    pyautogui.click(iptvsptransmit)
+                    pyautogui.keyDown('ctrl')
+                    pyautogui.press('v')
+                    pyautogui.keyUp('ctrl')
+                    time.sleep(0.3)
+
+                    playlistButton = pyautogui.locateCenterOnScreen('references/playlistbutton.png', confidence=0.8)
+                    pyautogui.click(playlistButton)
+                    time.sleep(0.3)
+
+                    pyautogui.keyDown('win')
+                    pyautogui.press('1')
+                    pyautogui.keyUp('win')
+                    
+                    # sairButton = pyautogui.locateCenterOnScreen('references/sairButton.png')
+                    # pyautogui.click(sairButton)
+                    # time.sleep(1)
+                    
+                    print("Programação concluída com sucesso! ft. Vitão, Agnaldão and Betão.")
+                    pyautogui.alert(text="Programação concluída com sucesso! ft. Vitão, Agnaldão and Betão.")
+            
 
 scheduler = sched.scheduler(time.time, time.sleep)
 t = time.strptime("2021-02-"+dia+" "+horario+":00", '%Y-%m-%d %H:%M:%S')
